9cbdf43a1d25220fcb695f1611f63f2257c65740
jaewon Jang <dodo4421@naver.com>
2023-11-28 11:23:57 +0900

    topiclist 활성화

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <h1 className='text-3xl font-bold'> 장재원 DB</h1>
       <p className='mb-4'>배워보자</p>
-      {/* <TopicsList /> */}
+      <TopicsList />
     </>
   )
 }
